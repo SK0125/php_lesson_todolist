7770ec3ca79401eb887eaa7b3cc9a7502c00c8cb
DESKTOP-ST37M0L\katos <ka.suhei.to@gmail.com>
2017-10-26 18:06:11 +0900

    lesson19

--- store.php
+++ store.php
@@ -4,7 +4,10 @@ require('functions.php');
 //create($_POST);
 //checkReferer();
 $res = checkReferer();
-if($res == 'index') {
+
+if($res != 'back'){
+  header('location: ./index.php');
+} elseif($res == 'index') {
   header('location: ./index.php');
 } else {
   header('location: '.$_SERVER['HTTP_REFERER'].'');
